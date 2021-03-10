ed7111646733cbd45d72d90d1e53b82873ff7089
1660392hcmus <nguyennt@zigvy.com>
2021-03-10 13:37:12 +0700

    fix config port on app

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ app.get('/', function(req, res) {
     res.render('pages/index');
 });
 
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(PORT, function () {
+  console.log(`Example app listening on port ${PORT}!`);
 });
