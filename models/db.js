1fd6fad51b4e7952ecd06720559a1e3e29cc5f3d
Tài Nguyên Nguyễn <nguyennt@zigvy.com>
2021-04-06 22:14:03 +0700

    config db

--- models/db.js
+++ models/db.js
@@ -2,7 +2,7 @@ const kenx = require("knex");
 
 const db = kenx({
     client: "pg",
-    connection: process.env.DATABASE_URL || 'postgres://postgres:12345@localhost:5432/1660392-db',
+    connection: process.env.HEROKU_POSTGRESQL_NAVY_URL || 'postgres://postgres:12345@localhost:5432/1660392-db',
   });
 
 module.exports = db;
