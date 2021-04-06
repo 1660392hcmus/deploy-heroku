46c58af754e96da84c94447cd82b52aabd22dedd
Tài Nguyên Nguyễn <nguyennt@zigvy.com>
2021-04-06 22:31:47 +0700

    add ssl config

--- models/db.js
+++ models/db.js
@@ -3,7 +3,7 @@ const kenx = require("knex");
 const db = kenx({
     client: "pg",
     connection: process.env.HEROKU_POSTGRESQL_NAVY_URL || 'postgres://postgres:12345@localhost:5432/1660392-db',
-    ssl: true,
+    ssl: { rejectUnauthorized : false }
   });
 
 module.exports = db;
